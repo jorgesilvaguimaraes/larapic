c91f24307171eb94fd05f55dfa06f689d3d2e307
Jorge Guimaraes <jorginhoad@gmail.com>
2019-01-30 18:08:56 -0300

    criacao de components

--- src/App.vue
+++ src/App.vue
@@ -1,31 +1,44 @@
 <template>
-  <div>
-    <h1 v-text="titulo"></h1>
-    <ul>
-      <li v-for="foto of fotos">
-        <img :src="foto.url" :alt="foto.titulo"/>
-      </li>      
-    </ul>            
+
+  <div class="corpo">
+    <menu-menu  :rotas="routes" />
+    <transition name="pagina">
+      <router-view></router-view>
+    </transition>
   </div>
 </template>
 
 <script>
+import { routes } from './routes.js';
+import Menu from './components/shared/menu/Menu.vue';
+
 export default {
   data(){
     return {
-      titulo : 'Alurapic',
-      fotos : []  
-    }    
+      routes
+    }
   },
 
-  created(){
-    this.$http.get('http://localhost:3000/v1/fotos')
-      .then(res => res.json())
-      .then(fotos => this.fotos = fotos, err => console.log(err));
+  components : {
+    'menu-menu' : Menu
   }
 }
 </script>
 
 <style>
+  .corpo {
+    font-family: Helvetica, sans-serif;
+    width: 96%;
+    margin: 0 auto;
+  }
 
+    .pagina-enter, .pagina-leave-active {
+        opacity: 0;
+    }
+
+    .pagina-enter-active, .pagina-leave-active {
+        transition: opacity 2s;
+    }
+
+ 
 </style>
